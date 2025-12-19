2a6f869b6e1fdba54fc3f59c7c252988f1ee2eb5
pwajp <guset2justsystem@gmail.com>
2025-12-19 17:25:49 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,4 @@
-const CACHE_NAME = 'my-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html'
-];
-
-// インストールイベント
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// フェッチイベント（ネットワークリクエストを横取り）
+// インストールを可能にするための最低限の記述
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // キャッシュがあればキャッシュを返す
-        if (response) {
-          return response;
-        }
-        // キャッシュがなければネットワークから取得
-        return fetch(event.request);
-      })
-  );
+  // ここにキャッシュ処理などを記述できます
 });
