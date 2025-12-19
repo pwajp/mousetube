5eaae16991033b3d48ac381f7ee1f2cd3b58d14a
pwajp <guset2justsystem@gmail.com>
2025-12-19 17:40:51 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,25 @@
-const CACHE_NAME = 'my-site-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/images/icon-192x192.png'
+var CACHE_NAME = 'mtube-caches';
+var urlsToCache = [
+	'/pwa/',
 ];
 
-// インストール時にキャッシュする
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+// インストール処理
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches
+			.open(CACHE_NAME)
+			.then(function(cache) {
+				return cache.addAll(urlsToCache);
+			})
+	);
 });
 
-// リクエスト時にキャッシュがあればそれを返す
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // キャッシュにあればそれを返す、なければネットワークへ
-        return response || fetch(event.request);
-      })
-  );
+self.addEventListener('fetch', function(event) {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then(function(response) {
+				return response ? response : fetch(event.request);
+			})
+	);
 });
