a8d666547e93c17f2b270e95e925ecf04f60cfbf
pwajp <guset2justsystem@gmail.com>
2025-12-19 17:33:10 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,28 @@
-// インストールを可能にするための最低限の記述
-self.addEventListener('fetch', (event) => {
-  // ここにキャッシュ処理などを記述できます
+const CACHE_NAME = 'my-site-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/images/icon-192x192.png'
+];
+
+// インストール時にキャッシュする
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエスト時にキャッシュがあればそれを返す
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュにあればそれを返す、なければネットワークへ
+        return response || fetch(event.request);
+      })
+  );
 });
